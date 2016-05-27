4c998c2022d45659540162e8d1997b8dc5aa48f1
Murugesh Annadurai <murugesh.annadurai@hotmail.com>
2016-05-27 07:29:59 -0700

    changed error type to dynamic object

--- LyftSDK.Net/Models/Error.cs
+++ LyftSDK.Net/Models/Error.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 using Newtonsoft.Json;
 
 namespace LyftSDK.Net.Models
@@ -9,9 +10,25 @@ namespace LyftSDK.Net.Models
         public string ErrorMessage { get; set; }
 
         [JsonProperty("error_detail")]
-        public IList<ErrorDetail> ErrorDetail { get; set; }
+        public dynamic ErrorDetail { get; set; }
 
         [JsonProperty("error_description")]
         public string ErrorDescription { get; set; }
+
+        public override string ToString()
+        {
+            var error = new StringBuilder();
+
+            if(!string.IsNullOrWhiteSpace(ErrorMessage))
+                error.AppendFormat("Error: {0}", ErrorMessage).AppendLine();
+
+            if (!string.IsNullOrWhiteSpace(ErrorDescription))
+                error.AppendFormat("ErrorDescription: {0}", ErrorDescription).AppendLine();
+
+            if(ErrorDetail != null)
+                error.AppendFormat("ErrorDetail: {0}", ErrorDetail.ToString()).AppendLine();
+
+            return error.ToString();
+        }
     }
 }
