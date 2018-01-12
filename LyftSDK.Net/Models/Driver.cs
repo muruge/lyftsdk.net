83e421fc567063d588c98e77ff5978d0412d2dea
MSTEAM\dchepilov <dchepilov@themsteam.com>
2018-01-12 15:58:35 +0700

    extend model

--- LyftSDK.Net/Models/Driver.cs
+++ LyftSDK.Net/Models/Driver.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Newtonsoft.Json;
 
 namespace LyftSDK.Net.Models
@@ -6,6 +7,19 @@ namespace LyftSDK.Net.Models
     public class Driver
     {
         [JsonProperty("locations")]
+		[Obsolete("Use class DriverLocation")]
         public IList<Location> Locations { get; set; }
+
+		[JsonProperty("phone_number")]
+		public string PhoneNumber { get; set; }
+
+		[JsonProperty("rating")]
+		public object Rating { get; set; }
+
+		[JsonProperty("first_name")]
+		public string FirstName { get; set; }
+
+		[JsonProperty("image_url")]
+		public string ImageUrl { get; set; }
     }
 }
