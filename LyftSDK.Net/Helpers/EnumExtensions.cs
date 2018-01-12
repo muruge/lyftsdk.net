20e4e5a0948a59657efd498d1f4445a129fa7b63
MSTEAM\dchepilov <dchepilov@themsteam.com>
2018-01-12 15:50:23 +0700

    resharper

--- LyftSDK.Net/Helpers/EnumExtensions.cs
+++ LyftSDK.Net/Helpers/EnumExtensions.cs
@@ -11,7 +11,7 @@ namespace LyftSDK.Net.Helpers
             var attributes = (DescriptionAttribute[]) fi.GetCustomAttributes(
                 typeof (DescriptionAttribute), false);
 
-            if (attributes != null && attributes.Length > 0)
+            if (attributes.Length > 0)
                 return attributes[0].Description;
 
             return source.ToString();
